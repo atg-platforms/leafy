e4408e7096184f15b2d2c37cafeb44059b477b56
Mark Fernandez <mfernandez@AEV-MFERNANDEZ.local>
2021-09-06 08:26:30 +0800

    Fix env variables

--- backend/nodejs/index.js
+++ backend/nodejs/index.js
@@ -1,13 +1,16 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 var express = require("express");
 var app = express(),
     mysql = require('mysql'),
     bodyParser = require('body-parser');
 
-const username = process.env.DB_USER || '';
-const password = process.env.DB_PASS || '';
-const dbhost = process.env.DB_DBHOST || ';
-const dbport = process.env.DB_DBPORT || '';
-const dbname = process.env.DB_NAME || '';
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const dbhost = process.env.DB_DBHOST;
+const dbport = process.env.DB_DBPORT;
+const dbname = process.env.DB_NAME;
 
 db = mysql.createConnection({
 host: dbhost,
@@ -16,16 +19,14 @@ password: password,
 database: dbname
 });
 
-const accountsRouter = require('./routes/accounts');
-const transactionsRouter = require('./routes/transactions');
+const farmer = require('./models/farmer');
+//const transactionsRouter = require('./routes/transactions');
 
 app.use(bodyParser.json());
 
-app.use('/accounts', accountsRouter);
-app.use('/transactions', transactionsRouter);
-app.use('/coins', erc20tokenRouter);
-
-
+app.use('/farmer', farmer);
+//app.use('/transactions', transactionsRouter);
+//app.use('/coins', erc20tokenRouter);
 
 
 app.listen(3000, () => {
